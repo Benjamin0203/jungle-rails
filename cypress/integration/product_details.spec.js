5ba9bda5b53e45b5d91304f4bdd1795bce4379b3
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-12-20 23:51:42 -0800

    Adds product_details tests and passes all the tests

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,20 @@
+describe('visit the home page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  cy.get(".products article").first().click();
+  cy.get(".product-detail").should("be.visible");
+  cy.get(".product-detail h1").should("have.text", "Cliff Collard");
+
+  cy.get(".product-detail img").should("have.attr", "src", "/uploads/product/image/12/plante_12.jpg");
+  cy.get(".product-detail .quantity").should("have.text", "\n        23 in stock at \n        $79.99\n      ");
+  cy.get(".product-detail .button_to").should("have.text", "\n           Add\n");
+
+  });
+
+
+  
+})
